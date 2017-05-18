929a74742ffbdb107938bbb51bfb4c2bdaaa6ee4
ehsan <ehsan.shahshahani@gmail.com>
2017-05-18 20:12:14 +0430

    1st commit

---
+++ scripts/app/controller/main.js
@@ -0,0 +1,89 @@
+app
+    .controller('MainController', function ($scope, $http, RestService, $mdDialog) {
+
+        let personId = 'a';
+
+        $scope.data = {};
+        $scope.search = {};
+        $scope.reasons = ['تعداد جوابهای صحیح بسیار زیاد است', 'پرس و جو ابهام دارد'];
+        $scope.PREFIX = 'http://194.225.227.161/mapping/html/triple.html?subject=';
+
+        $scope.checkAll = function (status) {
+            for (let item of $scope.data.searchResults) {
+                if (!item.fixed)
+                    item.relevant = status;
+            }
+        };
+
+        $scope.next = function () {
+            RestService.next(personId)
+                .then(function (response) {
+                    $scope.data = response.data;
+                });
+        };
+
+        $scope.submit = function (status, reason) {
+
+            let judgmentList = $scope.data.searchResults
+                .map(function (item) {
+                    return {
+                        'answer': item.title,
+                        'relevant': item.relevant
+                    }
+                });
+
+            let checked = judgmentList.filter(function (item) {
+                return item.relevant !== undefined;
+            });
+
+            if (status && checked.length !== judgmentList.length) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element('body'))
+                        .clickOutsideToClose(true)
+                        .title('توجه')
+                        .textContent('لطفا وضعیت همه موارد را تعیین کنید!')
+                        .ariaLabel('توجه')
+                        .ok('خب')
+                    //.targetEvent(ev)
+                );
+                return;
+            }
+
+            let data = {
+                "judgmentList": judgmentList,
+                "personId": personId,
+                "query": {"q": $scope.data.query.q},
+                "status": {
+                    "ableToAnswer": status,
+                    "reson": status ? '' : reason
+                }
+            };
+
+            console.log(data);
+
+            RestService.submit(data)
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        };
+
+        $scope.search = function () {
+            RestService.search($scope.search.keyword)
+                .then(function (response) {
+                    $scope.search.results = response.data;
+                });
+        };
+
+        $scope.add = function (entity) {
+            $scope.data.searchResults.push({link: entity.link, title: entity.title, relevant: "true", fixed: true});
+        };
+
+        $scope.remove = function (item) {
+            _.remove($scope.data.searchResults, item);
+        };
+
+        // on load do the followings
+
+        $scope.next();
+    });
---
+++ scripts/app/service/rest.js
@@ -0,0 +1,86 @@
+app.service('RestService', ['$http', function ($http) {
+    var baseURl = 'http://194.225.227.161:8096';
+
+    var self = this;
+    this.ingoing = 0;
+
+    self.init = function (rootAddress) {
+        baseURl = rootAddress;
+    };
+
+    function onerror(response) {
+        loading.hide();
+        self.ingoing--;
+        console.log('error : ', response);
+        return response;
+    }
+
+    function onsuccess(response) {
+        loading.hide();
+        self.ingoing--;
+        return response;
+    }
+
+    function get(url, params, headers) {
+        params = params || {};
+        params.random = new Date().getTime();
+
+        var req = {
+            method: 'GET',
+            url: url,
+            headers: headers,
+            params: params
+        };
+
+        self.ingoing++;
+        loading.show();
+        return $http(req).then(onsuccess, onerror);
+    }
+
+    function post(url, data, headers) {
+        var req = {
+            method: 'POST',
+            url: url,
+            headers: headers,
+            data: data
+        };
+
+        self.ingoing++;
+        loading.show();
+
+        return $http(req).then(onsuccess, onerror);
+        //return $http.post(url, data, headers).then(onsuccess, onerror);
+    }
+
+    /**/
+
+    this.next = function (personId) {
+        var url = baseURl + '/rest/v1/evaluation/next';
+        var params = {"personId": personId};
+        return get(url, params, {});
+    };
+
+    this.submit = function (userResponse) {
+        let url = baseURl + '/rest/v1/evaluation/submit';
+        let params = {userResponse: userResponse};
+        return post(url, params, {});
+    };
+
+    this.search = function (keyword) {
+        let url = 'http://194.225.227.161:8093' + '/rest/v1/searcher/search';
+        let params = {keyword: keyword};
+        return get(url, params, {});
+    };
+
+}]);
+
+var loading = {
+    show: function () {
+        $('#loading').remove();
+        $('body').append('<div id="loading" class="loading"><span>در حال تبادل اطلاعات ...</span></div>');
+        $('#loading').fadeIn();
+    },
+    hide: function () {
+        $('#loading').fadeOut();
+    }
+};
